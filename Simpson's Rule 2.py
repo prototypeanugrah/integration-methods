80351506cd54032411202d6fdfd6d95b62f6bae6
Anugrah( a.k.a Max) <prototype.anugrah@gmail.com>
2018-07-17 13:36:10 +0530

    Create Simpson's Rule 2.py

---
+++ Simpson's Rule 2.py
@@ -0,0 +1,21 @@
+def f(x):
+  return (x**2 - 3*x)
+
+def solve(a,b,n):
+  h = float(b-a)/n
+  i = 1
+  s = f(a)
+  
+  if(n%3 == 0):
+    
+    while(i <= (n/3)):
+        s = s + 3*f(a + (3*i-2)*h) + 3*f(a + (3*i-1)*h) + 2*f(a + 3*i*h)
+        i = i+1 
+  
+  else:
+    print("enter n divisible by 3")
+    exit(0)
+  
+  s = s - f(a + 3*(i-1)*h)
+  return abs(3*h*s/8)
+  
